b69290abef79526609de36abd2f2193a6ef28274
Jasun-Maharjan <jsnmhrjn@gmail.com>
2026-09-05 10:20:51 +0545

    made file uploader

--- application.py
+++ application.py
@@ -7,3 +7,19 @@ Matplotlib: Grpahs
 OpenAI: Connect to LLM
 '''
 
+import streamlit as sl
+import pandas as pd
+import matplotlib.pyplot as plt
+
+sl.set_page_config(
+    page_title = "AI-CSV",
+    layout = "wide"
+)
+
+sl.title("AI-CSV")
+sl.subheader("AI-powered Data Analysis Assistant")
+
+file = sl.file_uploader(
+    "Upload a CSV file",
+    type=["csv"]
+)
